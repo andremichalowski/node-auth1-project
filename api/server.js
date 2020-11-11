d526f4a7a48d25f43b53c46a18a2e0104a29360a
amichalo421 <andre.michalowski@gmail.com>
2020-11-10 23:02:21 -0500

    Sessions

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require('../auth/auth-router.js');
@@ -7,8 +8,21 @@ const protected = require('../auth/protected-mw.js');
 
 const server = express();
 
+const sessionConfiguration = { // 2. CONFIGURE
+	name: 'monster', //defaults to sid 
+	secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+  cookie: {
+    httpOnly: true, // true means JS can't access the cookie data
+		maxAge: 1000 * 60 * 10, // expires after 10 mins
+		secure: process.env.SECURE_COOKIES || false, //true meaning send cookies over https onlyd
+  },
+	resave: false, // re save the session information even if there are no changes
+	saveUninitialized: true, // read about GDPR compliance
+}
+
 server.use(express.json());
 server.use(helmet()) 
+server.use(session(sessionConfiguration));
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", protected, usersRouter);
